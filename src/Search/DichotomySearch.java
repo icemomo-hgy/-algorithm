1ce949a07fc337b4e5f952de57c69e6f46ca45d6
icemomo-hgy <i.gy@outlook.com>
2022-05-22 15:28:54 +0800

    v2

---
+++ src/Search/DichotomySearch.java
@@ -0,0 +1,10 @@
+package Search;/**
+* @BelongsProject: 数据结构
+* @BelongsPackage: Search
+* @Auther:  i.gy@outlook.com
+* @Date: 2022/5/22 15:00
+* @since JDK 1.8 
+* @Description: 
+*/
+public class DichotomySearch {
+}
